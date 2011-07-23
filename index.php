3ee591f1244a0fee40d7910c20769709d3c5f4e3
Mindaugas Dargis <mindzee2000@yahoo.com>
2011-07-24 02:36:42 +0300

    nesvarbu

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
 <?php
+/**
+ * Main application entry file
+ */
 
+// Error reporting set to highest level available
 error_reporting(E_ALL | E_STRICT);
 
+// Adding root directory to php include path
 set_include_path(
     realpath(dirname(__FILE__)) . PATH_SEPARATOR .
     get_include_path());
 
-require_once('controller/Controller.php');
-
-\myne\controller\Controller::run();
+// FrontController starts the application process by invoking run() method
+require_once('controller/FrontController.php');
+\myne\controller\FrontController::run();
