873b5dece30418b246c64ebbefe6f0516b732090
Mindaugas Dargis <mindzee2000@yahoo.com>
2011-07-24 04:59:15 +0300

    nesvarbu

---
+++ controller/ControllerMap.php
@@ -0,0 +1,66 @@
+<?php
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace myne\controller;
+
+/**
+ * Description of ControllerMap
+ *
+ * @author Mindaugas Dargis
+ */
+class ControllerMap
+{
+    private $_viewMap = array();
+    
+    private $_forwardMap = array();
+    
+    private $_classrootMap = array();
+    
+    public function addClassroot($command, $classroot)
+    {
+        $this->_classrootMap[$command] = $classroot;
+    }
+    
+    public function getClassroot($command)
+    {
+        if (isset($this->_classrootMap[$command]))
+        {
+            return $this->_classrootMap[$command];
+        }
+        
+        return $command;
+    }
+    
+    public function addView($view, $command = 'default', $status = 0)
+    {
+        $this->_viewMap[$command][$status] = $view;
+    }
+    
+    public function getView($command, $status)
+    {
+        if (isset($this->_viewMap[$command][$status]))
+        {
+            return $this->_viewMap[$command][$status];
+        }
+        
+        return null;
+    }
+    
+    public function addForward($command, $newCommand, $status = 0)
+    {
+        $this->_forwardMap[$command][$status] = $newCommand;
+    }
+    
+    public function getForward($command, $status)
+    {
+        if (isset($this->_forwardMap[$command][$status]))
+        {
+            return $this->_forwardMap[$command][$status];
+        }
+        
+        return null;
+    }
+}
